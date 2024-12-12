f6f9dfef76694ee844b658bd890c189d01789c02
Rynaldins <rynaldins@gmail.com>
2024-12-12 13:52:30 +0700

    revisi conflict

--- fungsi.js
+++ fungsi.js
@@ -2,7 +2,6 @@ const add = (a, b) => a + b;
 console.log(add(1,2));
 
 const subtract = (a, b) => a - b;
-<<<<<<< HEAD
 console.log(subtract(5,2));
 
 const multiply = (a, b) => a * b;
@@ -11,6 +10,3 @@ console.log(multiply(5,2));
 
 const divide = (a, b) => a / b;
 console.log(divide(10,2));
-=======
-console.log(subtract(5,2));
->>>>>>> parent of f56d1f4 (menambahkan fitur perkalian)
